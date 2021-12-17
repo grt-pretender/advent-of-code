c0fea5e8106e3c8dd426874e1bdd15f7abdf7161
Daria <dmetafrasis@gmail.com>
2021-12-18 02:22:23 +0700

    Add file

---
+++ rust/day 6_2.rs
@@ -0,0 +1,32 @@
+//--- Day 6: Lanternfish ---
+// --- Part Two ---
+// Suppose the lanternfish live forever and have unlimited food and space. 
+// Would they take over the entire ocean?
+
+
+fn main() {
+    let data = part_2//(INSERT INPUT DATA);
+    println!("The answer is {}", data);
+
+}
+
+fn part_2(input: &str) -> u64 {
+    
+    let mut count_fish = [0; 9];
+    
+    input
+        .split(',')
+        .map(|n| n.parse::<usize>().unwrap())
+        .for_each(|n| count_fish[n] += 1);
+        
+    for _days in 0..256 {
+        count_fish.rotate_left(1);
+        count_fish[6] += count_fish[8];
+            }
+        
+    count_fish
+        .iter()
+        .sum()
+    }
+
+
